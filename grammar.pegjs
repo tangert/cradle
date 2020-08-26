ffae4650c6fd748b69b33aafedf1388c1702d0c9
Tyler Angert <tyler@angert.com>
2020-08-26 00:47:24 +0000

    updated grammar file

---
+++ grammar.pegjs
@@ -0,0 +1,122 @@
+{
+  function flatten(arr) {
+    return arr.reduce(function (flat, toFlatten) {
+      return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+    }, []);
+  }
+}
+
+start = Graph
+
+// BUG: the last node in teh sequence is getting iti's first letter cut off
+// Finally build the graph
+// This is where the magic happens!
+
+Graph "graph" = children: (Group / Sequence)+ {
+    return {
+    	ast: children
+    }
+ } 
+
+// ONCE AGAIN THE PROBLEM IS NESTED BRANCHES
+//Group "group" = children: (Node?_ (GroupOpener _ (((Group",") / Group) / ((Sequence",") / Sequence))* _ GroupCloser) _) {
+Group "group" = children: (Node?_ (GroupOpener _ ( ((Group/Sequence)","_) / (Group/Sequence)_)* _ GroupCloser) _) {
+	  return {
+    	type: "group",
+        start: children[0],
+        children: flatten(children.slice(1)).filter(c => c
+        										  && c.type !== 'whitespace' 
+        										  && c.type !== 'groupOpener'
+                                                  && c.type !== 'groupCloser'
+                                                  && c !== ',')
+    }
+}
+
+// Either seprate them by commas or just have a regular unit
+Sequence "sequence"  = children:(NodeEdgeUnit _)+ {
+	return {
+    	type: "sequence",
+        // should this be responsibile for combing the liist into  one thing?
+        // probably
+        children: flatten(children.flat().filter(c => c.type !== 'whitespace').map(c => c.children))
+    }
+}
+
+GroupOpener = char: "{" {
+	return {
+    	type: "groupOpener",
+      content: char
+    }
+}
+
+GroupCloser = char: "}" {
+	return {
+    	type: "groupCloser",
+      content: char
+    }
+}
+
+NodeEdgeUnit "unit" = unit:(_ Node _ Edge*) {
+	return {
+    	type: 'nodeEdgeUnit',
+    	children: unit.filter(n => n.type !== 'whitespace')
+    }
+}
+
+Edge "edge" = edge:(LabeledEdge / UnlabeledEdge) {
+  return edge
+}
+
+// Edges
+// Labeled edges wrap arrows in parens and let you write text
+LabeledEdge "labeled edge" = content:"("label:Node edge:UnlabeledEdge")" {
+  return {
+        type: "edge",
+        kind: edge.kind,
+        content: edge.content,
+        label: label.content
+    }
+}
+
+UnlabeledEdge "unlabeled edge" = edge:(BidirectionalEdge/ForwardEdge/BackwardEdge) {
+  return edge 
+}
+
+BidirectionalEdge "bidirectional edge" = content:"<->" {
+  return { type: "edge", kind: "bi", content } 
+}
+
+ForwardEdge "forward edge" = content:"->" {
+  return { type: "edge", kind: "forward", content } 
+}
+
+BackwardEdge "backward edge" = content:"<-" {
+  return { type: "backward", kind: "backward", content } 
+}
+
+Node "node" = content:(Word InterwordWs)+ {
+  return {
+      type: "node",
+      content: content.map(c => c[0] + c[1]).join('').trim()
+    }
+}
+
+// One level up
+InterwordWs "interword whitespace" = ws:_ {
+  return ws.content.join("")
+}
+
+// Basics
+Word "word" = letters:letter+ {
+  return letters.join("")
+}
+
+letter "letter" = [A-Za-z0-9]
+
+// We return an object so we can easily filter
+_ "whitespace" = content: [ \t\n\r]* {
+	return {
+    	type: "whitespace",
+      content
+    }
+}
--- index.js
+++ index.js
@@ -1,153 +1,9 @@
 const peg = require("pegjs")
+const fs = require("fs")
 const log = (s) => console.log(s)
 
-
-/*
-- NODES are any piece of text
-  - Nodes can be referenced by their full name
-  - Their children can be accessed through dot notation.
-  - In the future, you'll be able to give aliases to nodes, and access them through array indices
-
-- EDGES are arrows between pieces of text.
-  - Edges can also contain LABELS.
-
-All text around arrows are nodes.
-If you want to label an edge, you wrap it in parentheses.
-*/
-
-// https://pegjs.org/online
-// https://pegjs.org/documentation
-// This grammar 
-// TODO: figure out how to get nested LISTS of branches working 
-const daggerGrammer = `
-start = Graph
-
-// BUG: the last node in teh sequence is getting iti's first letter cut off
-// Finally build the graph
-// This is where the magic happens!
-
-Graph "graph" = children: (Sequence / Group)* {
-    return {
-    	ast: children
-    }
- } 
-
-// ONCE AGAIN THE PROBLEM IS NESTED BRANCHES
-Group "group" = children: (Node?(_ GroupOpener (Group / Sequence ","*)+ GroupCloser _)+) {
-	  return {
-    	  type: "group",
-        // the first flat removes any whitespace or unneeded stuff from the filter
-        children: children.flat()
-                        .filter(c => c && c.type !== 'whitespace' && c.type !== 'branch')
-                        // the second flat turns all of the sequences into one list
-                        .flat()
-    }
-}
-
-// Either seprate them by commas or just have a regular unit
-Sequence "sequence"  = children:((NodeEdgeUnit+",")* (NodeEdgeUnit+_)_) {
-	return {
-    	type: "sequence",
-        // should this be responsibile for combing the liist into  one thing?
-        // probably
-        children: children.flat().filter(c => c.type !== 'whitespace')
-    }
-}
-
-GroupOpener = char: "{" {
-	return {
-    	type: "group",
-      kind: "opener",
-      content: char
-    }
-}
-
-GroupCloser = char: "}" {
-	return {
-    	type: "group",
-      kind: "closer",
-      content: char
-    }
-}
-
-NodeEdgeUnit "unit" = unit:(_ Node _ Edge*) {
-	return {
-    	type: 'nodeEdgeUnit',
-    	children: unit.filter(n => n.type !== 'whitespace')
-    }
-}
-
-Edge "edge" = edge:(LabeledEdge / UnlabeledEdge) {
-  return edge
-}
-
-// Edges
-// Labeled edges wrap arrows in parens and let you write text
-LabeledEdge "labeled edge" = content:"("label:Node edge:UnlabeledEdge")" {
-  return {
-        type: "edge",
-        kind: edge.kind,
-        content: edge.content,
-        label: label.content
-    }
-}
-
-UnlabeledEdge "unlabeled edge" = edge:(BidirectionalEdge/ForwardEdge/BackwardEdge) {
-  return edge 
-}
-
-BidirectionalEdge "bidirectional edge" = content:"<->" {
-  return { type: "edge", kind: "bi", content } 
-}
-
-ForwardEdge "forward edge" = content:"->" {
-  return { type: "edge", kind: "forward", content } 
-}
-
-BackwardEdge "backward edge" = content:"<-" {
-  return { type: "backward", kind: "backward", content } 
-}
-
-Node "node" = content:(Word InterwordWs)+ {
-  return {
-      type: "node",
-      content: content.map(c => c[0] + c[1]).join('').trim()
-    }
-}
-
-// One level up
-InterwordWs "interword whitespace" = ws:_ {
-  return ws.content.join("")
-}
-
-// Basics
-Word "word" = letters:letter+ {
-  return letters.join("")
-}
-
-letter "letter" = [A-Za-z0-9]
-
-// We return an object so we can easily filter
-_ "whitespace" = content: [ \\t\\n\\r]* {
-	return {
-    	type: "whitespace",
-      content
-    }
-}`
-
-const daggerParser = peg.generate(daggerGrammer);
-
+const daggerParser = peg.generate(fs.readFileSync("grammar.pegjs", "utf8"))
 const sampleInput = `step 1 -> step 2 (on ->) step 3`
-
-// want to cover this syntax:
-// google -> oauth
-// github -> oauth
-// facebook -> oauth
-// { google, github, facebook } -> oauth
-// basically a "reverse tree"
-// { google, github, facebook } -> { tech companies, employers, public}
-// need to cover naked transitions
-
 const signupFlows = `
 root {  
   sign up {
@@ -185,8 +41,7 @@ root {
 
     }
   }
-}
-`
+}`
 
 const test2 = `
 root {
@@ -211,12 +66,28 @@ graph {
     otherwise {
     }
   }
-}
-`
+}`
 
-log("input: " + sampleInput)
-const ast = daggerParser.parse(test2)
-log(test2)
-log(ast)
+const test4 = `
+root {
+ 1 {
+  11 {
+   111
+  },
+ },
+ 2 {
+  21 {
+   211
+  },
+  22 {}
+ },
+}`
+
+const test5 = `sweet`
+const test6 = `r{1{},2{3{}}}`
+
+const parsed = daggerParser.parse(test3)
+log(test3)
+log(parsed)
 // cool, now you have the AST!
 // now you can create a walking functioin like anything else
